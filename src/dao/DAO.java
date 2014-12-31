109fc8b08a1bb63513db732803c5976fedb1b942
Raphael Bernardo <raphaelbgr@gmail.com>
2014-12-31 13:43:02 -0200

    Redo import,

--- src/dao/DAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import sendable.Message;
-
-public interface DAO {
-	public boolean send(Message m) throws Throwable;
-	public Message receive() throws Throwable;
-	public Message AssembleNormalMessage();
-}
