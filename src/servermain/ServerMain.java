8fbd1f365bff3ac562629b7b09f99766b0387f9b
Raphael Bernardo <raphaelbgr@gmail.com>
2015-04-27 10:50:31 -0300

    Port settings
    Added support to launch server using, java -jar Server.jar -p 2000, to
    select port to be used. 2000 is default if no port selected.

--- src/servermain/ServerMain.java
+++ src/servermain/ServerMain.java
@@ -2,26 +2,31 @@ package servermain;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-import sendable.Client;
 import sync.MessageCenter;
 import threads.ConnectionHandlerThread;
 
 public class ServerMain {
-	public final static int PORT = 2000;
+	public static int PORT = 2000;
 	public static final int VERSION = 13;
 	public static MessageCenter mc = new MessageCenter();
-	@SuppressWarnings("unused")
-	private List<Client> clientList = new ArrayList<Client>(); 
 
 	public static void main(String[] args) {
-		ConnectionHandlerThread ch = new ConnectionHandlerThread(PORT);
-		Thread t1 = new Thread(ch);
-		t1.start();
-		System.out.println(getTimestamp() + " " + "SERVER> Listening on port " + PORT);
+		if (args.length == 0) {
+			ConnectionHandlerThread ch = new ConnectionHandlerThread(PORT);
+			Thread t1 = new Thread(ch);
+			t1.start();
+			System.out.println(getTimestamp() + " " + "SERVER> Listening on port " + PORT);
+		} else if (args[0].equalsIgnoreCase("-p") && Integer.valueOf(args[1]) > 0 && Integer.valueOf(args[1]) <= 65535) {
+			PORT = Integer.valueOf(args[1]);
+			ConnectionHandlerThread ch = new ConnectionHandlerThread(PORT);
+			Thread t1 = new Thread(ch);
+			t1.start();
+			System.out.println(getTimestamp() + " " + "SERVER> Listening on port " + PORT);
+		} else {
+			System.out.println("Invalid argument, usage -p followed by a valid port number.");
+		}
 	}
 
 	public static String getTimestamp() {
