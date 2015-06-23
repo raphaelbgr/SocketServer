ca98386179852025c4f2dd0790e42483ca167dc9
Raphael Bernardo <raphaelbgr@gmail.com>
2015-06-23 00:51:40 -0300

    Implementing are you alive system

---
+++ src/sendable/messages/AreYouAlive.java
@@ -0,0 +1,11 @@
+package sendable.messages;
+
+public class AreYouAlive extends Message {
+	
+	private static final long serialVersionUID = 5313134685853376703L;
+	
+	public AreYouAlive() {
+		
+	}
+	
+}
